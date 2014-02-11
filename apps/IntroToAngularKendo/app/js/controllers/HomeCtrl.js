14233f947abc69bde998efacd073b979de2aded2
kevin bui <kevindatb@gmail.com>
2014-02-10 23:49:05 -0800

    intro to angular kendo - adding files for first time

---
+++ apps/IntroToAngularKendo/app/js/controllers/HomeCtrl.js
@@ -0,0 +1,29 @@
+﻿app.controller('HomeCtrl', function HomeCtrl($scope) {
+    $scope.thingsOptions = {
+        dataSource: {
+            data: [{ name: "Thing 1", id: 1 },
+                   { name: "Thing 2", id: 2 },
+                   { name: "Thing 3", id: 3 }]
+        },
+        dataTextField: "name",
+        dataValueField: "id",
+        animation: {
+            open: {
+                effects: "zoom:in",
+                duration: 300
+            }
+        },
+        height: 2000,
+        autoBind: false,
+        text: "Select A Thing...",
+    };
+    
+    $scope.thingsChange = function (e) {
+    };
+    
+    $scope.window = {
+        open: function () {
+            $scope.modal.center().open();
+        }
+    };
+});
